c0a651dffe2bfd75972744e0835f0869ca629def
WarthogTK <tracrium@gmail.com>
2023-09-18 12:22:37 +0200

    First commit

---
+++ Generate-ADName.ps1
@@ -0,0 +1,45 @@
+param (
+    [string]$filePath,
+    [string]$outFile
+)
+
+
+    if (Test-Path -Path $filePath -PathType Leaf) {
+        $usernames = Get-Content -Path $filePath
+
+        foreach ($line in $usernames) {
+            $nameParts = $line -split ' '
+            if ($nameParts.Count -eq 2) {
+                $firstName = $nameParts[0].ToLower()
+                $lastName = $nameParts[1].ToLower()
+
+                $usernamesList = @(
+                    "${firstName}${lastName}",
+                    "${lastName}${firstName}",
+                    "${firstName}.${lastName}",
+                    "${lastName}.${firstName}",
+                    "${lastName}$($firstName[0])",
+                    "${firstName}$($lastName[0])",
+                    "$($firstName[0]).$lastName",
+                    "$($firstName.Substring(0,3))$($lastName.Substring(0,3))",
+                    "$($firstName.Substring(0,3)).$($lastName.Substring(0,3))",
+                    "$($firstName[0])$lastName",
+                    "${lastName}.$($firstName[0])",
+                    "$($lastName.Substring(0,3))_$($firstName.Substring(0,3))",
+                    "$($firstName.Substring(0,3))_$($lastName.Substring(0,3))",
+                    "$firstName",
+                    "$lastName",
+                    "$($lastName[0]).$firstName",
+                    "$($lastName[0])$firstName"
+                )
+                if ($outFile){
+                    $usernamesList | ForEach-Object { Write-Host "  $_" ; Add-Content -Path $outFile $_}                
+                }
+                $usernamesList | ForEach-Object { Write-Host "  $_" }
+            } else {
+                Write-Host "This line '$line' has not the format 'firstname lastname'."
+            }
+        }
+    } else {
+        Write-Host "File '$filePath' doesn't exist."
+    }
